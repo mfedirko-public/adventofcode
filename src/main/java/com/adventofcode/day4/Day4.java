c8ccf1187e882a7e8465796ed0fbe21d50edf268
cat <cat@cat.cat>
2020-12-04 15:42:09 -0600

    use single stream for day4

--- src/main/java/com/adventofcode/day4/Day4.java
+++ src/main/java/com/adventofcode/day4/Day4.java
@@ -8,6 +8,7 @@ import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
@@ -16,17 +17,18 @@ import java.util.stream.StreamSupport;
 public class Day4 {
     public static void main(String[] args) throws IOException {
         InputStream file = Day4.class.getResourceAsStream("/day4.txt");
-        List<Passport> validPassports =
-                FileStreamSupport.toStream(file, "\n\n")
-                .map(s -> s.split("\\s"))
-                .map(s -> Arrays.stream(s).map(e -> e.split(":")).collect(Collectors.toMap(e -> e[0], e -> e[1])))
-                .map(Passport::new)
-                .filter(Passport::isValid)
-                .collect(Collectors.toList());
+        AtomicInteger validCountPt1 = new AtomicInteger();
+        AtomicInteger validCountPt2 = new AtomicInteger();
+        FileStreamSupport.toStream(file, "\n\n")
+            .map(s -> s.split("\\s"))
+            .map(s -> Arrays.stream(s).map(e -> e.split(":")).collect(Collectors.toMap(e -> e[0], e -> e[1])))
+            .map(Passport::new)
+            .forEach(p -> {
+                if (p.isValid()) validCountPt1.incrementAndGet();
+                if (p.isValidStrict()) validCountPt2.incrementAndGet();
+            });
 
-        int validCountPt1 = validPassports.size();
-        int validCountPt2 = (int)validPassports.stream().filter(Passport::isValidStrict).count();
-        System.out.printf("Valid passport count: %d\nValid passport count (pt2): %d\n", validCountPt1, validCountPt2);
+        System.out.printf("Valid passport count: %d\nValid passport count (pt2): %d\n", validCountPt1.get(), validCountPt2.get());
 
     }
 }
